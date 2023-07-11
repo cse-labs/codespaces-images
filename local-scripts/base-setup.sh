2d96437b44f0263bb1ceb0b7e4e9f34be9c68e02
bartr <bartr@microsoft.com>
2023-07-11 20:11:02 +0000

    updated build

--- local-scripts/base-setup.sh
+++ local-scripts/base-setup.sh
@@ -4,9 +4,24 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 
-RUN apt-get update
-RUN apt-get -y install --no-install-recommends apt-utils dialog apt-transport-https ca-certificates curl git wget openssl
-RUN apt-get -y install --no-install-recommends software-properties-common make build-essential jq bash-completion gettext iputils-ping dnsutils
+apt-get update
+apt-get -y install --no-install-recommends apt-utils dialog apt-transport-https ca-certificates curl git wget openssl
+apt-get -y install --no-install-recommends software-properties-common make build-essential jq bash-completion gettext iputils-ping dnsutils
+
+# install github cli
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059
+apt-add-repository https://cli.github.com/packages
+
+# install fluent bit for debugging
+curl https://packages.fluentbit.io/fluentbit.key | gpg --dearmor > /usr/share/keyrings/fluentbit-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/fluentbit-keyring.gpg] https://packages.fluentbit.io/debian/buster buster main" >> /etc/apt/sources.list
+apt-get update
+
+apt-get -y install --no-install-recommends gh fluent-bit
+
+# install dotnet 3.1, 5, and 6 for tool support
+# dotnet 7 is already installed
+apt-get -y install --no-install-recommends dotnet-sdk-5.0 dotnet-sdk-6.0 dotnet-sdk-3.1
 
 # this fails the first time
 curl https://certs.godaddy.com/repository/gd_bundle-g2.crt
@@ -21,20 +36,4 @@ fi
 mkdir -p /usr/local/share/ca-certificates
 curl -Lo /usr/local/share/ca-certificates/gd_bundle-g2.crt https://certs.godaddy.com/repository/gd_bundle-g2.crt
 update-ca-certificates
-
-# install github cli
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-key 23F3D4EA75716059 && \
-    apt-add-repository https://cli.github.com/packages && \
-    apt-get update && \
-    apt-get -y install --no-install-recommends gh
-
-# install fluent bit for debugging
-curl https://packages.fluentbit.io/fluentbit.key | gpg --dearmor > /usr/share/keyrings/fluentbit-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/fluentbit-keyring.gpg] https://packages.fluentbit.io/debian/buster buster main" >> /etc/apt/sources.list
-apt-get update
-apt-get -y install --no-install-recommends fluent-bit
-
-# install dotnet 3.1, 5, and 6 for tool support
-# dotnet 7 is already installed
-apt-get -y install --no-install-recommends dotnet-sdk-5.0 dotnet-sdk-6.0 dotnet-sdk-3.1
 
